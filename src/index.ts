61e78dc12d75af80ddfa59896b9fe976ba6356c3
yaouba <kabayyaouba@gmail.com>
2023-07-15 13:48:14 +0100

    install and set up swagger

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,28 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
+import express, { Express } from 'express';
+import connectDB from './config/db.config';
+import morgan from 'morgan';
+import swaggerUi from 'swagger-ui-express';
 
-dotenv.config();
+import { RegisterRoutes } from './routes/routes';
 
 const app: Express = express();
 const port = process.env.PORT || 3600;
 
-app.get('/', (req: Request, res: Response) => {
-	res.send('Express + TypeScript Server');
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('tiny'));
+app.use(express.static('public'));
 
-app.listen(port, () => {
-	console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-});
+import swaggerDocument from './routes/swagger.json';
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+RegisterRoutes(app);
+const start = async () => {
+	await connectDB();
+
+	app.listen(port, () => {
+		return console.log(`Express is listening at http://localhost:${port}`);
+	});
+};
+
+start();
